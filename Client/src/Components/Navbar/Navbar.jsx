d1ee7c156323c6c87f5b52887f43b0146c26f114
ravi ranjan <ravi848101mnb@gmail.com>
2024-07-29 23:02:17 +0530

    chore: Update Navbar component with responsive menu and styling

--- Client/src/Components/Navbar/Navbar.jsx
+++ Client/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css';
 
 const Navbar = () => {
     const getNavLinkClass = ({ isActive }) => {
@@ -6,8 +10,15 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-gray-800 p-4">
-            <ul className="flex space-x-4">
+        <nav className="bg-gray-900 p-4  w-full  z-50">
+            <div className="flex justify-between items-center">
+                <div className="text-white text-xl"><Link to={'/'}>Gym</Link></div>
+                <div className="lg:hidden">
+                    <button onClick={toggleNavbar}>
+                        {isOpen ? <FaTimes className="text-white text-2xl z-20" /> : <FaBars className="text-white text-2xl z-20" />}
+                    </button>
+                </div>
+                <ul className="hidden lg:flex space-x-4">
                 <li>
                     <NavLink
                         to="/"
@@ -34,10 +45,10 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink
-                        to="/posture"
+                            to="/planner"
                         className={getNavLinkClass}
                     >
-                        Pose Tracking
+                            Planner
                     </NavLink>
                 </li>
             </ul>
